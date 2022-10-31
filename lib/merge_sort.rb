7a21db7a8d261d7bf50aff961c179305a93e7a5b
jacksoze <kamonyojack@gmail.com>
2022-10-31 17:46:34 +0300

    Create the merge_sort and merge methods

---
+++ lib/merge_sort.rb
@@ -0,0 +1,44 @@
+def merge_sort(array)
+  n = array.length
+  if n < 2
+    array
+  else
+    left_array = array[0...n/2]
+
+    right_array = array[n/2...n]
+    left = merge_sort(left_array)
+    right = merge_sort(right_array)
+    merge(left, right, array)
+    p array
+  end
+
+end
+
+def merge(left,right,array)
+  index_left = 0
+  l = left.length
+  r = right.length
+  n = array.length
+  index_right = 0
+  index_array = 0
+  while index_left < left.length && index_right < right.length
+    if left[index_left] < right[index_right]
+      array[index_array] = left[index_left]
+      index_left += 1
+      p array
+    else
+      array[index_array] = right[index_right]
+      index_right += 1
+    end
+    index_array += 1
+  end
+  if index_left == left.length
+    array[index_array...n] = right[index_right...r]
+  else
+    array[index_array...n] = left[index_left...l]
+  end
+end
+
+
+array = [4,5,8,7,4,1]
+merge_sort(array)
